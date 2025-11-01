54144bd94107b4d0441dcae2d78067990b0144b9
muhammed-shuhaib <muhammedshuhaibpkdv@gmail.com>
2025-11-01 07:51:36 +0530

    completed adding to list.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css'
+import {useState} from 'react'
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+
+  console.log(toDos)
 
   return (
     <div className="app">
@@ -13,19 +18,25 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=> setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=> setToDos([...toDos, toDo])} className="fas fa-plus"></i>
       </div>
-      <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+      <div className="todos">       
+        {
+          toDos.map((value)=> {
+            return(
+              <div className="todo">
+                <div className="left">
+                  <input type="checkbox" name="" id="" />
+                  <p>{value}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          })
+        } 
       </div>
     </div>
   );
